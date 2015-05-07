56facc56a20736ee9253bef85a7f9a200a5c3a4e
mawnoy <puff_dragon_6@hotmail.com>
2015-05-07 07:06:20 +0700

    Skill section

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,27 @@
+'use strict';
 
+// Function Pallarax scroll
+// Ref - https://shifteleven.com/articles/2007/06/28/array-like-objects-in-javascript/
+
+(function() {
+
+	var parallax = document.querySelectorAll('.top-header'),
+		speed = .5;
+
+	window.onscroll = function() {
+		
+		// [].slice.call(parallax) > make obj to Array like obj to use with forEach.
+		[].slice.call(parallax).forEach(function(el, i) {
+
+			
+			var windowOffset = window.pageYOffset,
+				elBackgroundPos = "50%" + (windowOffset * speed) + "px";
+
+			el.style.backgroundPosition = elBackgroundPos;
+			// console.log(el.style.backgroundPosition);
+
+		});
+	};
+	
+
+})();
